cbb8960be310eeac379ab744d7054e07e2ea556a
jtabaj <70162987+jtabaj@users.noreply.github.com>
2023-11-10 20:19:22 -0300

    Export tabla oradores

---
+++ oradores.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-11-2023 a las 00:09:56
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `integrador_cac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `oradores`
+--
+
+CREATE TABLE `oradores` (
+  `id_orador` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `tema` varchar(255) DEFAULT NULL,
+  `fecha_alta` date DEFAULT sysdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `oradores`
+--
+
+INSERT INTO `oradores` (`id_orador`, `nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES
+(1, 'Juan', 'Perez', 'jperez@gmail.com', 'javascript', '2023-11-10'),
+(2, 'Juan', 'Lopez', 'jlopez@gmail.com', 'HTML', '2023-11-10'),
+(3, 'Steve', 'Jobs', 'Jobs@gmail.com', 'React', '2023-11-10'),
+(4, 'Bill', 'Gates', 'Gates@gmail.com', 'React', '2023-11-10'),
+(5, 'Ada', 'Lovelace', 'jLovelace@hotmail.com', 'Negocios', '2023-11-10');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `oradores`
+--
+ALTER TABLE `oradores`
+  ADD PRIMARY KEY (`id_orador`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `oradores`
+--
+ALTER TABLE `oradores`
+  MODIFY `id_orador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
